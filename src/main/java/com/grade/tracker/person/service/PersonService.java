e65fa5c47f3f58be3037169b49ee402221bffdbc
hedhili <medhedhili6@gmail.com>
2022-11-01 16:43:41 +0100

    first commit project grade-tracker

--- src/main/java/com/grade/tracker/person/service/PersonService.java
+++ src/main/java/com/grade/tracker/person/service/PersonService.java
@@ -5,8 +5,6 @@ import com.grade.tracker.person.dto.PersonResponseDto;
 import com.grade.tracker.shared.exception.InternalException;
 
 
-import java.util.List;
-
 public interface PersonService <T extends PersonRequestDto , U extends PersonResponseDto>{
     U save(final T personRequestDto) throws InternalException;
 
